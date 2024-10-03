1179a4c05fe835ad60136e0979edbff4642b0e41
Kelaven <kevin.lavenant@live.fr>
2024-10-03 15:36:13 +0200

    feat(hero): Moon rotation and animation added

--- src/components/Moon.js
+++ src/components/Moon.js
@@ -1,12 +1,34 @@
 'use client';
 
 import { Sphere } from '@react-three/drei';
+import { useEffect, useRef } from 'react';
+import gsap from "gsap";
+
 
 function Moon() {
+    const sphereRef = useRef(null) // accès à la sphère
+    console.log(sphereRef);
+
+    useEffect(() => {
+        // const tl = gsap.timeline({ defaults: { duration: 1 } })
+
+        gsap.fromTo(sphereRef.current.scale, {
+            x: 0,
+            y: 0,
+            z: 0
+        }, {
+            x: 1,
+            y: 1,
+            z: 1,
+            duration: 1
+        });
+
+    }, []);
+
 
 
     return (
-        <Sphere args={[3, 64, 64]}>
+        <Sphere args={[3, 64, 64]} ref={sphereRef}>
             <meshStandardMaterial color="#00ff83" />
         </Sphere>
     )
@@ -15,6 +37,6 @@ function Moon() {
 export default Moon;
 
 
-// * 1 Création de la sphère : 2 pour la taille, 64 pour la width et height afin d'avoir une balle parfaite
+// * 1 Création de la sphère : 3 pour la taille, 64 pour la width et height afin d'avoir une balle parfaite
 
 // * 2 Travail de la sphère qui est brute : avec <meshStandardMaterial>
--- src/components/ThreeScene.js
+++ src/components/ThreeScene.js
@@ -7,11 +7,11 @@ import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 
 function ThreeScene() {
     return (
-        <Canvas>
+        <Canvas dpr={[1, 2]}>
             <PerspectiveCamera makeDefault fov={45} position={[0, 0, 20]} />
             <pointLight color={0xffffff} intensity={70} distance={100} position={[0, 10, 10]} />
             <Moon />
-            {/* <OrbitControls /> */}
+            <OrbitControls enableDamping={true} enablePan={false} enableZoom={false} autoRotate={true} autoRotateSpeed={5} />
         </Canvas>
     )
 }
@@ -21,11 +21,13 @@ export default ThreeScene;
 
 
 
-// * 1 Créer la scène avec <Canvas>
+// * 1 Créer la scène avec <Canvas>. dpr={[1, 2]} désigne le device pixel ratio, pour améliorer le rendu de la lune. le ratio est de 1 pour les écrans normaux et 2 pour les écrans 4K/retina (le ratio s'ajuste automatiquement selon l'appareil de l'utilisateur).
 
 // * 2 Ajouter la caméra, avec un champ de vision de 45 degrès pour une vue naturelle et réaliste sans distorsion, et un placement de 20 unités en arrière sur l'axe Z (pour ne pas être sur la même position que la lune)
 
-// * Ajouter la lumière ponctuelle (blanche, intensité, portée de 100 unités et décalage au-dessus et à droite de la scène). 
+// * 3 Ajouter la lumière ponctuelle (blanche, intensité, portée de 100 unités et décalage au-dessus et à droite de la scène). 
+
+// * 4 Si besoin ajouter un OrbitControls, enableDamping permet que le mouvement s'arrete de manière smooth
 
 
 
