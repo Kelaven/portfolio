a8cd213da7c8232ad0b458e5abe2eaabe2468e7b
Kelaven <kevin.lavenant@live.fr>
2024-10-07 15:26:06 +0200

    feat(Header): Improve cards

--- src/components/layout/header/CardHeader.js
+++ src/components/layout/header/CardHeader.js
@@ -1,19 +1,43 @@
 'use client';
 
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-function CardHeader({ endCard }) {
-    console.log(endCard);
-    return (
-        <>
-            {
-                endCard ? (<div className='bg-black min-w-64 md:w-2/4 lg:w-1/4 h-1/2 2xl:h-3/4 ms-6 me-6 lg:mx-auto' >
-                    Carte
-                </div>) : (<div className='bg-black min-w-64 md:w-2/4 lg:w-1/4 h-1/2 2xl:h-3/4 ms-6 lg:mx-auto'>
-                    Carte
-                </div>)
+function CardHeader({ neonColor }) {
+    const backColor = useRef(null);
+
+    useEffect(() => {
+        if (backColor.current) {
+            // Nettoyage des classes avant d'ajouter la bonne
+            backColor.current.classList.remove('neonWhite', 'neonYellow', 'neonGold');
+            switch (neonColor) {
+                case "white":
+                    backColor.current.classList.add('neonWhite')
+                    break;
+                case "yellow":
+                    backColor.current.classList.add('neonYellow')
+                    break;
+                case "darkgold":
+                    backColor.current.classList.add('neonGold')
+                    break;
+
+                default:
+                    break;
             }
-        </>
+        }
+    }, [neonColor])
+
+
+
+    return (
+        <div className='bg-black min-w-64 md:w-2/4 lg:w-1/4 h-1/2 2xl:h-3/4 ms-6 lg:mx-auto rounded-sm '>
+            <div className='w-full h-full preserve-3d shadow-lg rounded-sm'>
+                <div ref={backColor} className='back bg-[#151515] absolute w-full h-full backface-hidden rounded-sm overflow-hidden flex justify-center items-center'>
+                    <div className='absolute w-[99%] h-[99%] bg-[#151515] rounded-sm text-white flex flex-col justify-center items-center gap-[30px]'>
+                        IMAGE
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/layout/header/ModalHeader.js
+++ src/components/layout/header/ModalHeader.js
@@ -34,9 +34,9 @@ function ModalHeader({ openMenu }) {
     return (
         // <div ref={modal} className="modal absolute z-20 left-0 w-full h-screen bg-gray-500 opacity-0 flex whitespace-nowrap overflow-x-auto items-center justify-start lg:justify-center" inert>
         <div ref={modal} className="modal absolute z-20 top-0 left-0 w-full h-svh bg-gray-500 opacity-0 flex whitespace-nowrap overflow-x-auto items-center justify-start lg:justify-between pointer-events-none">
-            <CardHeader />
-            <CardHeader />
-            <CardHeader endCard={true} />
+            <CardHeader neonColor="white" />
+            <CardHeader neonColor="yellow" />
+            <CardHeader neonColor="darkgold" />
         </div>
     );
 }
