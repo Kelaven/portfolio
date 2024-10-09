91fbfef19918d4d2363dc14d6bab138a757f4bfb
Kelaven <kevin.lavenant@live.fr>
2024-10-09 17:35:31 +0200

    feat(AboutSection): work in progress

--- src/components/skills/Meteor.js
+++ src/components/skills/Meteor.js
@@ -1,12 +1,40 @@
+"use client";
+
 import { Decal, Float, Icosahedron, useTexture } from '@react-three/drei'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function Meteor({ img }) {
     const [decal] = useTexture([img]);
+    const meteorRef = useRef(null);
+    const [meteorSize, setMeteorSize] = useState(4.5);
+    const [logoSize, setLogoSize] = useState(4.5)
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 768) { // Mobile
+                setMeteorSize(3.5);
+                setLogoSize(4.5);
+            } else {
+                setMeteorSize(4.5);
+                setLogoSize(4.5);
+            }
+        };
+
+        // Appelle la fonction une première fois
+        handleResize();
+
+        // Event listener pour ajuster la taille lors du resize
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [])
+
 
     return (
-        <Float speed={2} rotationIntensity={1.5} floatIntensity={2}>
-            <Icosahedron args={[6, 1]} castShadow>
+        <Float speed={1} rotationIntensity={1} floatIntensity={2}>
+            <Icosahedron args={[meteorSize, 1]} castShadow ref={meteorRef}>
                 <meshStandardMaterial color="#b8860b"
                     flatShading={true} // Permet de rendre les facettes bien visibles
                     metalness={0.1} // léger effet métallique
@@ -16,7 +44,7 @@ function Meteor({ img }) {
                 <Decal
                     position={[0, 0, 5]}  // Position sur la face
                     rotation={[2 * Math.PI, 0, 6.25]}  // orientation
-                    scale={6}  // taille du logo
+                    scale={logoSize}  // taille du logo
                     map={decal}  // texture du logo
                 >
 
