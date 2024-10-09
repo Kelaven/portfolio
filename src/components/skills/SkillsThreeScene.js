ee3187195c5630bf606e1b48754250aac5aac149
Kelaven <kevin.lavenant@live.fr>
2024-10-09 21:14:36 +0200

    feat(AboutSection): i'm lovin my meteorite

--- src/components/skills/SkillsThreeScene.js
+++ src/components/skills/SkillsThreeScene.js
@@ -12,30 +12,27 @@ function SkillsThreeScene() {
             <PerspectiveCamera makeDefault fov={45} position={[0, 0, 20]} />
             {/* <ambientLight intensity={1} />
             <directionalLight position={[0, 0, 0.05]} /> */}
-            {/* Lumière ambiante pour adoucir l'éclairage global */}
-            <ambientLight intensity={0.6} />
 
+            {/* Lumière ambiante pour l'éclairage global */}
+            <ambientLight intensity={0.6} />
             {/* Lumière directionnelle avant pour éclairer l'objet par devant */}
             <directionalLight
                 position={[2, 2, 5]} // Lumière dirigée vers l'avant
                 intensity={2}
                 castShadow
             />
-
             {/* Lumière ponctuelle pour intensifier l'éclairage frontal */}
             <pointLight
                 position={[0, 0, 10]} // Devant l'objet pour éclairer l'avant
                 intensity={30}
                 distance={50}
             />
-
             {/* Lumière directionnelle supplémentaire pour éclairer l'arrière */}
             <directionalLight
                 position={[-2, 2, -5]} // Derrière l'objet
-                intensity={1.5} // Intensité modérée pour ne pas suréclairer l'arrière
+                intensity={1.5}
                 castShadow
             />
-
             {/* Option supplémentaire : Lumière hémisphérique pour un éclairage global */}
             <hemisphereLight
                 skyColor={"#ffffff"} // Lumière venant du haut
