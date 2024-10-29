b0fdd25e091cb188161900e5761956ae488f9c48
Kelaven <kevin.lavenant@live.fr>
2024-10-29 21:35:54 +0100

    fix(ProjectsSectionCard.js): file updated to fetch images

--- src/components/sections/projectsSection/ProjectsSectionCard.js
+++ src/components/sections/projectsSection/ProjectsSectionCard.js
@@ -75,7 +75,7 @@ function ProjectsSectionCard({ onLoad }) {
                     </div>
                     <div className='flex-1 flex justify-center pt-6 md:pt-0'> {/* Conteneur de l'image */}
                         <Image
-                            src={`https://api.kevin-lavenant.fr/uploads/images/${project.picture}`}
+                            src={`https://api.kevin-lavenant.fr/${project.picture}`}
                             alt="Photo de présentation du projet"
                             width={500}
                             height={500}
