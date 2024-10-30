e13e0f7e00b0507f48562bcd1cea33de814fce6b
Kelaven <kevin.lavenant@live.fr>
2024-10-30 10:07:41 +0100

    fix(ProjectsSection.js):padding patched

--- src/components/sections/projectsSection/ProjectsSection.js
+++ src/components/sections/projectsSection/ProjectsSection.js
@@ -47,7 +47,7 @@ function ProjectsSection() {
 
     return (
         <>
-            <div className='relative min-h-200vh w-full bg-black px-4 md:px-12 lg:px-24 pt-12'>
+            <div className='relative min-h-200vh w-full bg-black px-4 md:px-12 lg:px-24 pt-12 pb-6'>
                 <div className="blurred-background absolute top-[2.5%] left-[3%] lg:top-[5%] lg:left-[7%] w-[200px] h-[200px] xl:w-[300px] xl:h-[300px] z-0"></div>
                 <div className="blurred-background absolute top-[25%] left-[25%] w-[250px] h-[400px] md:w-[0px] md:h-[0px] z-0"></div>
                 <div className="blurred-background absolute top-[45%] left-[70%] lg:top-[45%] lg:right-[6%] w-[250px] h-[400px] lg:w-[350px] lg:h-[500px] z-0"></div>
@@ -71,7 +71,6 @@ function ProjectsSection() {
                     </div>
                 )} */}
             </div>
-            <div className='h-10 bg-black'></div>
             {/* <div className='h-28 md:h-80'></div> */}
         </>
     )
